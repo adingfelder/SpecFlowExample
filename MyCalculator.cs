15dc86fa6fe973da2c9c74e14cb2731ef37a0990
Andrew Jun <idjun123@gmail.com>
2019-08-13 14:46:29 +1200

    fix

---
+++ MyCalculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class Calculator
+    {
+        public List<int> numbers = new List<int>();
+
+        public int total;
+        public Calculator() { total = 0; }
+
+        public int Add()
+        {
+            foreach (int number in numbers)
+            {
+                total += number;
+            }
+            return total;
+        }
+
+        public int Subtract()
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (count == 0)
+                {
+                    total += number;
+                    count++;
+                }
+                else
+                {
+                    total -= number;
+                }
+            }
+            return total;
+
+        }
+
+        public int Multiply()
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (count == 0)
+                {
+                    total += number;
+                    count++;
+                }
+                else
+                {
+                    total *= number;
+                }
+            }
+            return total;
+        }
+
+        public int Divide()
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (count == 0)
+                {
+                    total += number;
+                    count++;
+                }
+                else
+                {
+                    try
+                    {
+                        total /= number;
+                    }
+                    catch (Exception e)
+                    {
+                        e = new Exception("no");
+                    }
+                }
+            }
+            return total;
+        }
+    }
+}

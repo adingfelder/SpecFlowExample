8f46439e82e9374bdf55d5137edd543b38a41977
Andrew Jun <idjun123@gmail.com>
2019-08-14 11:55:30 +1200

    added customer for address

---
+++ PageObjects/CustomerDetails/Customer1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSpecFlow.PageObjects.CustomerDetails
+{
+    class Customer1
+    {
+        String firstName = "Andrew";
+        String lastName = "Daniels";
+        String company = "Planit";
+        String address = "13 Armstrong street";
+        String city = "Houston";
+        String state = "Texas";
+        String zipCode = "00000";
+        String mobile_num = "213012301230";
+        String address_assign = "My 2nd Address";
+    }
+}

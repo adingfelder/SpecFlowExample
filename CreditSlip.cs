f26141c53587f0fa7016a64a044042deab2378d3
tchiang <tchiang@NZAKLLP26>
2019-08-14 11:42:40 +1200

    changes

---
+++ CreditSlip.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSpecFlow
+{
+    class CreditSlip
+    {
+        private IWebDriver driver;
+
+        [Test(Description = "Check for the credit slips button")]
+        public void ClickPaySlip()
+        {
+            driver.FindElement(By.Name("My credit slips"));
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            driver.Close();
+        }
+
+    }
+}
+

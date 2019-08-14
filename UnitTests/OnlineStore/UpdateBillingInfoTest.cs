a9af6d723601d11a9d954ea77dc88c292bfe7b4f
Daniel Hodgkinson <dhodgkinson@planittesting.com>
2019-08-14 12:02:51 +1200

    Changes

---
+++ UnitTests/OnlineStore/UpdateBillingInfoTest.cs
@@ -0,0 +1,98 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using UnitTestSpecFlow.PageObjects.CustomerDetails;
+namespace SimpleSeleniumExample
+{
+
+
+    [TestFixture]
+    public class UpdateBillingInfo
+    {
+        private IWebDriver driver;
+        public string homeURL;
+
+
+        [Test(Description = "Check SauceLabs Homepage for Login Link")]
+        public void UpdateBillingInfoTest()
+        {
+
+
+            homeURL = "http://automationpractice.com/index.php";
+            driver.Navigate().GoToUrl(homeURL);
+            driver.FindElement(By.LinkText("Sign in")).Click();
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+            driver.FindElement(By.Id("passwd")).Clear();
+            driver.FindElement(By.Id("passwd")).SendKeys("123456");
+            driver.FindElement(By.Id("SubmitLogin")).Click();
+            driver.FindElement(By.XPath("//span[contains(text(),'My addresses')]")).Click();
+            driver.FindElement(By.XPath("//span[contains(text(),'Add a new address')]")).Click();
+            //Firstname
+            driver.FindElement(By.Id("firstname")).Click();
+            driver.FindElement(By.Id("firstname")).Clear();
+            driver.FindElement(By.Id("firstname")).SendKeys(Customer1.);
+            //Lastname
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+            //Company
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+            //Address
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+            //City
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+            //State
+            driver.FindElement(By.Id("email")).Click();
+            //PostalCode
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+            //Country
+            driver.FindElement(By.Id("email")).Click();
+            //Mobilephone
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+            //AdressAssignment
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+
+
+        }
+
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            driver.Close();
+        }
+
+
+        [SetUp]
+        public void SetupTest()
+        {
+            homeURL = "http://SauceLabs.com";
+            driver = new ChromeDriver();
+
+        }
+
+
+    }
+
+
+}

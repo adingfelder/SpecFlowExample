8f43c4cbe7399695004e20b67a2605adddcd1126
Andrew Dingfelder <adingfelder@planittesting.com>
2019-08-15 11:49:57 +1200

    moving stuff to contact page model

---
+++ PageObjects/OnlineStore/ContactPage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace SeleniumShoppingCart.ScreenModel
+{
+    class ContactPage
+    {
+
+        //        private string baseURL = "http://www.automationpractice.com";
+
+        By customerButton = By.XPath("//option[contains(text(),'Customer service')]");
+        By IdContact = By.Id("id_contact");
+        By contactLink = By.Id("contact-link");
+
+
+        public void ClickContactPage(IWebDriver driver)
+        {
+            driver.FindElement(contactLink).Click();
+
+        }
+
+        public void ClickCustomerButton(IWebDriver driver)
+        {
+            driver.FindElement(customerButton).Click();
+        }
+
+
+        public void ClickIdContact(IWebDriver driver)
+        {
+            driver.FindElement(IdContact).Click();
+        }
+
+
+        public void enterEmail(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Clear();
+
+            // todo - dont hard code email to disney???
+            driver.FindElement(By.Id("email")).SendKeys("WaltDisney@Disney.com");
+        }
+
+
+
+
+        /*
+        public void GotoURL(IWebDriver driver)
+        {
+            try
+            {
+                driver.Navigate().GoToUrl(baseURL);
+                driver.Manage().Window.Maximize();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        */
+
+    }
+}

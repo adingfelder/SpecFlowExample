2b3c7c12cebd38e9b9a3b091760d6ff7ba3d1c2f
Andrew Dingfelder <adingfelder@planittesting.com>
2019-08-15 16:09:33 +1200

    adding heroku app

---
+++ PageObjects/HerokuApp/HerokuAppHomePage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace HerokuApp.ScreenModel
+{
+
+    class HerokuAppHomePage
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /*
+        private By SignInButtonHomePage = By.LinkText("Sign in");
+        private By EmailTextBoxOnHomePage = By.Id("email");
+        private By PasswordTextBoxOnHomePage = By.Id("passwd");
+        private By SignInSubmitButon = By.Id("SubmitLogin");
+        private string testEmail = "seleniumtraining@gmail.com";
+        */
+
+        private string baseURL = "http://the-internet.herokuapp.com";
+        
+
+        public void GotoURL(IWebDriver driver)
+        {
+            log.Info("HomePage going to URL: "+ baseURL);
+            try {
+                driver.Navigate().GoToUrl(baseURL);
+                driver.Manage().Window.Maximize();
+            } catch (Exception e) {
+                log.Error("Error in HomePage going to URL: " + baseURL, e);
+            }
+        }
+        /*
+        public void ClickSignIn(IWebDriver driver)
+        {
+            log.Debug("HomePage clicking sign in button");
+            driver.FindElement(SignInButtonHomePage).Click();
+        }
+        public void EnterEmail(IWebDriver driver)
+        {
+            driver.FindElement(EmailTextBoxOnHomePage).Click();
+            driver.FindElement(EmailTextBoxOnHomePage).Clear();
+            driver.FindElement(EmailTextBoxOnHomePage).SendKeys(testEmail);
+        }
+
+        public void EnterPassword(IWebDriver driver)
+        {
+            driver.FindElement(PasswordTextBoxOnHomePage).Clear();
+            driver.FindElement(PasswordTextBoxOnHomePage).SendKeys("planit123");
+        }
+
+        public void ClickSignInButton(IWebDriver driver)
+        {
+            driver.FindElement(SignInSubmitButon).Click();
+        }
+
+    */
+    }
+}
---
+++ UnitTests/herokuapp/OpenHerokuapp.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using HerokuApp.ScreenModel;
+using System;
+
+namespace HerokuApp
+{
+    public class OpenHerokuapp
+    {
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        ChromeDriver globaldriver;  //= new ChromeDriver();
+        HerokuAppHomePage herokuHome = new HerokuAppHomePage();
+
+
+
+        [SetUp]
+        public void SetupTest()
+        {
+            // do some logging with our logger instead of console write
+            // for more info see:  https://stackify.com/log4net-guide-dotnet-logging/
+            log.Info("doing Herokuapp setup");
+            // homeurl is set in herokuHome class
+            globaldriver = new ChromeDriver();
+        }
+
+        [Test]
+        public void AndyTestLoadPage()
+        {
+            log.Info("doing HerokuappTestLoadPage");
+            herokuHome.GotoURL(globaldriver); // this loads the home url
+ //           _homepage.ClickSignIn(globaldriver);
+ //           Assert.AreEqual("planit training", _myAccountPage.getAccountName(globaldriver), "Account Name does not match");
+            globaldriver.Close();
+            log.Info("HerokuappTestLoadPage closed");
+
+        }
+
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            Console.WriteLine("doing HerokuappTestLoadPage teardown - quit");
+            globaldriver.Quit();
+            //agnes doesnt like not having a tick
+        }
+
+    }
+}
+

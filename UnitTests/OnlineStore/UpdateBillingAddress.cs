4abde9961e6cf717f1a89ed8ff483503448ece40
tchiang <tchiang@NZAKLLP26>
2019-08-15 15:10:34 +1200

    Starting UpdateBillingInfo

---
+++ UnitTests/OnlineStore/UpdateBillingAddress.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Chrome;
+using SeleniumShoppingCart.ScreenModel;
+using NUnit.Framework;
+using SeleniumShoppingCart;
+
+namespace UnitTestSpecFlow.UnitTests.OnlineStore
+{
+   
+    public class UpdateBillingAddress
+    {
+        
+        ChromeDriver globaldriver;
+        HomePage _homepage = new HomePage();
+        ViewDetailPage _detailpage = new ViewDetailPage();
+        SearchTShirts _searchTshits = new SearchTShirts();
+        
+
+        [SetUp]
+        public void SetUptest()
+        {
+            globaldriver = new ChromeDriver();
+        }
+
+        [Test]
+        public void UpdateBillingInfo()
+        {
+            _homepage.GotoURL(globaldriver);
+            _homepage.ClickSignIn(globaldriver);
+            _homepage.EnterEmail(globaldriver);
+            _homepage.EnterPassword(globaldriver);
+            _homepage.ClickSignInButton(globaldriver);
+            _homepage.GotoURL(globaldriver);
+            _detailpage.ClickMore(globaldriver);
+
+
+
+        }
+
+        
+        
+    }
+}

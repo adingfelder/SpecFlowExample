8414c14a1ab9a9ed7404113eb21008d92e73bf13
Sarah Zhang <szhang@planittesting.com>
2019-08-16 10:39:45 +1200

    com

--- PageObjects/HerokuApp/FileUpload.cs
+++ PageObjects/HerokuApp/FileUpload.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 namespace UnitTestSpecFlow.PageObjects.HerokuApp
 {
     /// <summary>
-    /// Summary description for DropDownBox
+    
     /// </summary>
     [TestClass]
     public class FileUpload

5d9ac18ae6b06fd5fc799f2fefb4489ca9c72b5f
tchiang <tchiang@NZAKLLP26>
2019-08-16 13:44:25 +1200

    BasicAuth-Agnes

---
+++ PageObjects/HerokuApp/BasicAuth.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSpecFlow.PageObjects.HerokuApp
+{
+    class BasicAuth
+    {
+        public By button = By.LinkText("Basic Auth");
+        
+
+
+
+        public void GoToBasicAuth(IWebDriver driver)
+        {
+            driver.FindElement(button).Click();
+        }
+
+        public void EnterAuth(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("username")).SendKeys("user");
+            driver.FindElement(By.Id("password")).SendKeys("admin");
+            driver.SwitchTo().Alert().Accept();
+            driver.SwitchTo().DefaultContent();
+            
+            
+        }
+    }
+}
---
+++ UnitTests/herokuapp/BasicAuthUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using UnitTestSpecFlow.PageObjects.HerokuApp;
+using HerokuApp.ScreenModel;
+using OpenQA.Selenium;
+
+namespace HerokuApp
+{
+    
+    [TestFixture]
+    public class BasicAuthUnitTest
+    {
+        ChromeDriver globaldriver;
+        HerokuAppHomePage _herokuApp = new HerokuAppHomePage();
+        BasicAuth _basicAuth = new BasicAuth();
+
+        [SetUp]
+        public void SetUp()
+        {
+            globaldriver = new ChromeDriver();
+        }
+
+        [Test]
+        public void BasicAuthTest()
+        {
+            _herokuApp.GotoURL(globaldriver);
+            _basicAuth.GoToBasicAuth(globaldriver);
+            Console.WriteLine("Click the Basic Auth Link");
+            _basicAuth.EnterAuth(globaldriver);
+            Console.WriteLine("You have entered the auth");
+            globaldriver.Close();
+            globaldriver.Quit();
+        }
+        
+    }
+}
